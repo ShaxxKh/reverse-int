3887e10090dcb09b17523efdbb8b594861044bd2
ShaxxKh <sop.ugc2@gmail.com>
2020-10-16 20:20:59 +0500

    feat: implemented the function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
+    let strNum = n.toString()
+    let notNegative = strNum
+    if(strNum[0] === '-'){
+        notNegative = strNum.slice(1)
+    }
+  var reversed = notNegative.split('').reverse()
+  let final = ''
+
+
+  for(var i = 0; i < reversed.length; i++){
+    final += reversed[i]
+  }
+  return final
 }
